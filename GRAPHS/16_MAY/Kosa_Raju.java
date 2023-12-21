d263a25241c1a3b9718ffcf2ca56c2343ce07d31
Shubham Singh <shubhamsingh1840@gmail.com>
2023-12-21 18:54:25 +0530

    code

--- GRAPHS/16_MAY/Kosa_Raju.java
+++ GRAPHS/16_MAY/Kosa_Raju.java
@@ -1,3 +1,50 @@
-public class Kosa_Raju {
-    
+import java.util.ArrayList;
+import java.util.Stack;
+
+// https://practice.geeksforgeeks.org/problems/strongly-connected-components-kosarajus-algo/1
+class Solution {
+    // Function to find number of strongly connected components in the graph.
+    public int kosaraju(int V, ArrayList<ArrayList<Integer>> graph) {
+        Stack<Integer> topologicalOrder = new Stack<>();
+        ArrayList<ArrayList<Integer>> invertedGraph = new ArrayList<>();
+        boolean[] visited = new boolean[V];
+
+        for (int index = 0; index < V; index++) {
+            if (!visited[index]) {
+                DFS(graph, visited, index, topologicalOrder);
+            }
+            invertedGraph.add(new ArrayList<>());
+        }
+
+        for (int index = 0; index < V; index++) {
+            for (Integer neighbour : graph.get(index)) {
+                invertedGraph.get(neighbour).add(index);
+            }
+        }
+
+        int count = 0;
+        visited = new boolean[V];
+
+        while (!topologicalOrder.isEmpty()) {
+            int element = topologicalOrder.pop();
+            if (!visited[element]) {
+                count = count + 1;
+                DFS(invertedGraph, visited, element, null);
+            }
+        }
+        return count;
+    }
+
+    public void DFS(ArrayList<ArrayList<Integer>> graph, boolean[] visited, int current, Stack stack) {
+        visited[current] = true;
+        for (Integer neighbour : graph.get(current)) {
+            if (!visited[neighbour]) {
+                DFS(graph, visited, neighbour, stack);
+            }
+        }
+        if (stack != null) {
+            stack.push(current);
+        }
+    }
+
 }
--- GRAPHS/16_MAY/Mother_Vertex.java
+++ GRAPHS/16_MAY/Mother_Vertex.java
@@ -1,3 +1,38 @@
-public class Mother_Vertex {
-    
+import java.util.ArrayList;
+import java.util.Stack;
+
+//https://practice.geeksforgeeks.org/problems/mother-vertex/1
+class Solution {
+    // Function to find a Mother Vertex in the Graph.
+    public int findMotherVertex(int V, ArrayList<ArrayList<Integer>> graph) {
+        boolean[] visited = new boolean[V];
+        Stack<Integer> stack = new Stack<>();
+        for (int index = 0; index < V; index++) {
+            if (!visited[index]) {
+                DFS(graph, visited, index, stack);
+            }
+        }
+        visited = new boolean[V];
+        Integer motherVertex = stack.pop();
+        DFS(graph, visited, motherVertex, null);
+
+        for (boolean isNodeVisited : visited) {
+            if (!isNodeVisited) {
+                return -1;
+            }
+        }
+        return motherVertex;
+    }
+
+    public void DFS(ArrayList<ArrayList<Integer>> graph, boolean[] visited, int current, Stack stack) {
+        visited[current] = true;
+        for (Integer neighbour : graph.get(current)) {
+            if (!visited[neighbour]) {
+                DFS(graph, visited, neighbour, stack);
+            }
+        }
+        if (stack != null) {
+            stack.push(current);
+        }
+    }
 }
