a0b133a56449a5552739e91af8accdef0a603754
Shubham Singh <shubhamsingh1840@gmail.com>
2024-05-22 22:29:59 +0530

    practice questions

--- DP/KNAPSACK/UNBOUNDED_KNAPSACK/Coin_Change.java
+++ DP/KNAPSACK/UNBOUNDED_KNAPSACK/Coin_Change.java
@@ -33,3 +33,49 @@ class Solution {
         return dp[n][amount] = Math.min(include, exclude);
     }
 }
+
+// with typical coin change approach
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        // int[] dp= new int[amount+1];
+        // Arrays.fill(dp, -1);
+        int ans = coinChange_TopDown(coins, amount);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public int coinChange_Memoised(int[] coins, int amount, int n, int[] dp) {
+        if (amount == 0) {
+            return dp[0] = 0;
+        }
+        if (dp[amount] != -1) {
+            return dp[amount];
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int index = 0; index < coins.length; index++) {
+            if (coins[index] <= amount) {
+                int coinCount = coinChange_Memoised(coins, amount - coins[index], n, dp);
+                ans = coinCount != Integer.MAX_VALUE ? Math.min(ans, 1 + coinCount) : ans;
+            }
+        }
+        return dp[amount] = ans;
+    }
+
+    public int coinChange_TopDown(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+
+        for (int amt = 1; amt <= amount; amt++) {
+            int ans = Integer.MAX_VALUE;
+            for (int index = 0; index < coins.length; index++) {
+                if (coins[index] <= amt) {
+                    int coinCount = dp[amt - coins[index]];
+                    ans = coinCount != Integer.MAX_VALUE ? Math.min(ans, 1 + coinCount) : ans;
+                }
+            }
+            dp[amt] = ans;
+        }
+        return dp[amount];
+    }
+}
